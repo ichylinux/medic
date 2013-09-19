f63c884215933b2949348e0297ba9b4ba23f5fd7
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-09-19 12:08:04 +0900

    初回コミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medic::Application.config.secret_token = 'aa2d0ef0f0e1e073fd29c04f4e903a0a3d962a55f7e7942e7ff9ceaba75a7bb7524761dc95c4386fcfa7b1b871014c35ce07d1bcced85473746d4b3e0bcc33ce'
