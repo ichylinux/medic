590e07e49268be9b11c06be952cb2c5f7fc4abed
ichy <ichylinux@gmail.com>
2013-10-19 10:22:21 +0900

    アップロード先を恒久的な場所に変更

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131018070816) do
+ActiveRecord::Schema.define(:version => 20131019011859) do
 
   create_table "families", :force => true do |t|
     t.datetime "created_at", :null => false
