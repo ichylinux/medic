b15c3d1f142d0f7afda0493a12cd5cd154c89504
ichy <ichylinux@gmail.com>
2013-11-15 12:02:53 +0900

    日記

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,12 +6,6 @@ class WelcomeController < ApplicationController
     condition = SealCondition.new
     condition.user_id = current_user.id
     @seals = Seal.search(condition)
-
-    @member = current_user.member
-    @family_members = []
-    if @member.family_member.member_type == "0"
-      @family_members = @member.family.members
-    end
   end
   
   def get_seals
