65dbfbe0386ae473e3a368c46c7ed7450ebbeb67
ichy <ichylinux@gmail.com>
2013-11-15 10:02:09 +0900

    日記

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
   $(document.body).delegate('form[action="/users"]', 'submit', function() {
     $(this).attr('data-ajax', false);
   });
+
+  $(document.body).delegate('form[action="/users/sign_out"]', 'submit', function() {
+    $(this).attr('data-ajax', false);
+    if (!confirm('ログアウトします。よろしいですか？')) {
+      return false;
+    }
+    
+    return true;
+  });
 });
