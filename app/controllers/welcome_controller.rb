f1670dc26c316ac74a274c749067fa1ddfe468d1
hiro <hiroyuki@hybitz.co.jp>
2013-11-15 10:46:43 +0900

    メンバーを選択できるように修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,19 @@ class WelcomeController < ApplicationController
     condition = SealCondition.new
     condition.current_user_id = current_user.id
     @seals = Seal.search(condition)
-  end  
+
+    @member = current_user.member
+    @family_members = []
+    if @member.family_member.member_type == "0"
+      @family_members = @member.family.members
+    end
+  end
+  
+  def get_seals
+    user_id = params[:user_id]
+    condition = SealCondition.new
+    condition.current_user_id = user_id
+    @seals = Seal.search(condition)
+  end
 
 end
