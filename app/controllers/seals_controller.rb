4bb6d81edc5b9faec07fd651671c3c5aa61f5caa
hiro <hiroyuki@hybitz.co.jp>
2013-11-20 21:12:10 +0900

    家族のシールを登録できる

--- app/controllers/seals_controller.rb
+++ app/controllers/seals_controller.rb
@@ -4,11 +4,17 @@ class SealsController < ApplicationController
 
   def new
     @seal = Seal.new
+    if current_user.member.family_owner?
+      @selected_id = params[:member_id]
+      @selected_id ||= current_user.member.id
+      @members = current_user.member.family.members
+      @members = nil unless @members.where('member_id <> ?', current_user.id).present?
+    end
   end
 
   def create
     @seal = Seal.new(params[:seal])
-    @seal.member_id = current_user.member.id
+    #@seal.member_id = current_user.member.id
     @seal.save!
     redirect_to root_path
   end
